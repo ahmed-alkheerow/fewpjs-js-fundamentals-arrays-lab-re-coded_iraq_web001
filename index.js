f6f95e4d57f913b37cc7edbd134c80304db1721f
Ahmed Al-kheerow <a.alkheerow@gmail.com>
2020-05-16 02:21:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your solution here!
+const append= ["Milo", "Otis", "Garfield"];
+const prepen= ["Milo", "Otis", "Garfield"];
+const removeLast= ["Milo", "Otis", "Garfield"];
+ const removeFirst= ["Milo", "Otis", "Garfield"];
+
+ append.append ("Odie")
